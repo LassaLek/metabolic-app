f63a0c6b9a9e58e1709ec6f73afaf8d5aba6a9a6
lassal <lassal@seznam.cz>
2019-06-08 23:23:30 +0200

    init counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +18,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    LocalNotifications,
+    Insomnia
   ],
   bootstrap: [AppComponent]
 })
--- src/app/counter/counter.page.ts
+++ src/app/counter/counter.page.ts
@@ -1,16 +1,45 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { startTimeRange } from '@angular/core/src/profile/wtf_impl';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.page.html',
   styleUrls: ['./counter.page.scss'],
 })
-export class CounterPage implements OnDestroy {
+export class CounterPage implements OnInit, OnDestroy {
   counter: number;
   timerRef;
   isRunning = false;
   startText = 'Start';
+  // TODO coefitients to enum
+  coefficient = 12;
+
+  private sub: Subscription;
+  constructor(private localNotifications: LocalNotifications,
+              private route: ActivatedRoute,
+              private insomnia: Insomnia) {
+
+  }
+
+  ngOnInit() {
+    this.insomnia.keepAwake()
+      .then(
+        () => console.log('success'),
+        () => console.log('error')
+      );
+
+    this.sub = this.route
+      .queryParams
+      .subscribe(params => {
+        // Defaults to 0 if no query param provided.
+        // TODO no magic string
+        this.coefficient = +params['ratio'] || 12;
+      });
+  }
 
   startTimer() {
     this.isRunning = !this.isRunning;
@@ -21,12 +50,20 @@ export class CounterPage implements OnDestroy {
       this.timerRef = setInterval(() => {
         this.counter = Date.now() - startTime;
       });
+
+
     } else {
       this.startText = 'Resume';
       clearInterval(this.timerRef);
       setTimeout(() => {
-        alert('Phosporus elapsed');
-      }, 12 * this.counter);
+        // Schedule a single notification
+        this.localNotifications.schedule({
+          id: 1,
+          text: 'Phosphorus',
+          // sound: isAndroid? 'file://sound.mp3': 'file://beep.caf',
+          sound: 'file://sound.mp3',
+        });
+      }, this.coefficient * this.counter);
     }
   }
 
@@ -38,6 +75,13 @@ export class CounterPage implements OnDestroy {
   }
 
   ngOnDestroy() {
+    this.sub.unsubscribe();
     clearInterval(this.timerRef);
+
+    this.insomnia.allowSleepAgain()
+      .then(
+        () => console.log('success'),
+        () => console.log('error')
+      );
   }
 }
